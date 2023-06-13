23da654c19d34ef758a30970d57f863f2a47a070
Preetha <preethapnair09@gmail.com>
2023-06-13 09:08:00 +0530

    first commit

---
+++ ValuesExample.java
@@ -0,0 +1,25 @@
+package JavaTraining;
+
+public class ValuesExample{
+    public static void main(String[] args){
+        boolean bool  ;
+        bool = true;
+        byte a = -97;
+        short b = 25000;
+        int c = 1256789;
+        long d = -23456789;
+        char e = 's';
+        float $floatValue = 12.43f;
+        double doubleValue = 12.4444444444;
+        System.out.println(bool);
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println($floatValue);
+        System.out.println(doubleValue);
+        System.out.println(Bank.printName());
+        System.out.println(Bank.printAddress());
+    }
+}
